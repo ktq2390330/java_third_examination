59a9fbab4ddc472c748411796500e165cec2d63e
ktq2390330 <ktq2390330@stu.o-hara.ac.jp>
2024-02-05 13:56:10 +0900

    com

--- only_for_third_exam/AdvancedCalculator.java
+++ only_for_third_exam/AdvancedCalculator.java
@@ -49,7 +49,7 @@ public class AdvancedCalculator extends Calculator implements CalculatorInterfac
         //メニューを表示
         System.out.println("電卓プログラム - 操作を選んでください:");
         System.out.println("1. 四則演算");
-        System.out.println("2. 累乗・べき乗");
+        System.out.println("2. 累乗");
         System.out.println("3. 対数");
         System.out.println("4. 三角関数");
         System.out.println("5. 平均値計算");
